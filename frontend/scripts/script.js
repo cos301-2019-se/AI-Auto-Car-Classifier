f1acfb87caf577e685ba2347e6f651ce352779f5
Andrew Jordaan <andyjordaan23@gmail.com>
2019-07-08 21:08:52 +0200

    Added functions to call classification API

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function ()
 {
+    /****************** Create Upload Form **********************/
+
     const uploadButton = document.querySelector('.browse-btn');
     const fileInfo = document.querySelector('.file-info');
     const realInput = document.getElementById('real-input');
@@ -20,6 +22,8 @@ $(document).ready(function ()
 
 
         var title = $('#title').val();
+
+        /************** Upload Images ***************/
 
         $('.uploadForm').ajaxSubmit({
             data: {title: title},
@@ -49,19 +53,121 @@ $(document).ready(function ()
     });
 
 
+/****************** Gallery Image Clicked *******************/
 
     $(document).on('click','.thumbnail',function()
     {
         console.log('Clicked');
         var src = $(this).children("img").attr("src");
+        var imageID = $(this).children("img").attr("id");
         displayImage(src);
 
         $("html, body").animate({ scrollTop: 0 }, 200);
+
+        classifyImage(imageID);
     });
 
 });
 
+function classifyImage(imageID)
+{
+    getProbability(imageID,function)
+}
 
+function getProbability(imageID,callBack)
+{
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/classify/car_detector",
+        dataType: "json",
+        data: {imageID:imageID},
+        success: function(res)
+        {
+            var prob = res.probability;
+
+           if(prob > 80)
+           {
+             getColour(imageID);
+           }
+           else
+           {
+               alert("Image is not a car (" + prob + ")")
+           }
+        },
+        error: function(jqXHR,exception)
+        {
+            console.log("Error in getting Colour: " + jqXHR.status);
+            return -1;
+        }
+    });
+}
+
+function getColour(imageID)
+{
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/classify/color_detector",
+        dataType: "json",
+        data: {imageID:imageID},
+        success: function(res)
+        {
+            var colour = res.color;
+            console.log("Car Colour: " + colour);
+
+            $('#colourItem').text(colour);
+
+            getMake(imageID);
+        },
+        error: function(jqXHR,exception)
+        {
+            console.log("Error in getting Colour: " + jqXHR.status);
+
+        }
+    });
+}
+function getMake(imageID)
+{
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/classify/car_classifier",
+        dataType: "json",
+        data: {imageID:imageID},
+        success: function(res)
+        {
+            var make = res.make;
+            var confidence = res.confidence;
+            console.log("Car Make: " + make);
+
+            $('#makeItem').text(make + " (" + confidence + "%)");
+
+            getNumberPlate(imageID);
+        },
+        error: function(jqXHR,exception)
+        {
+            console.log("Error in getting Colour: " + jqXHR.status);
+
+        }
+    });
+}
+function getNumberPlate(imageID)
+{
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/classify/number_plate",
+        success: function(res)
+        {
+            var plate = res.numberPlate;
+            console.log("Car Plate: " + plate);
+
+            $('#plateItem').text(plate);
+        },
+        error: function(jqXHR,exception)
+        {
+            console.log("Error in getting Colour: " + jqXHR.status);
+
+        }
+    });
+}
 function generateGallery(imagePaths)
 {
     var i;
@@ -92,7 +198,7 @@ function generateGallery(imagePaths)
 function generateImageHTML(image)
 {
     var path = '../backend/uploads/' + image;
-    var html = '<div class="col-md-4"> <div class="thumbnail"> <img src="' + path + '" alt="" style="width:100%"> </div> </div>';
+    var html = '<div class="col-md-4"> <div class="thumbnail"> <img id="'+image+'" src="' + path + '" alt="" style="width:100%"> </div> </div>';
 
     return html;
 }
