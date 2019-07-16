93a3962a436ba881529e055faaae7e503dc87f00
bestabhinav <37014190+bestabhinav@users.noreply.github.com>
2019-07-16 16:01:16 +0200

    Add files via upload

---
+++ frontend/src/login.js
@@ -0,0 +1,32 @@
+(function(){
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+         
+            return true;
+          },
+          uiShown: function() {
+
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        
+        signInFlow: 'popup',
+        signInSuccessUrl: 'index.html',
+        signInOptions: [
+         
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+     //     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+     //     firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+     //     firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+   
+        tosUrl: 'index.html'
+        // Privacy policy url.
+       
+      };
+ui.start('#firebaseui-auth-container', uiConfig);
+})()
