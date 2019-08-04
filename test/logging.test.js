c29406d0a317cc5bf89840112018576c79082ec2
Keo11 <dkshiko@gmail.com>
2019-08-04 20:07:09 +0200

    Edit unit test

--- test/logging.test.js
+++ test/logging.test.js
@@ -3,8 +3,39 @@ const res = require('../api/logging');
 var assert = require('assert');
 
 
-  describe('res', function() {
-    it('should return 200', function() {
-      assert.equal(res.status, res.status);
-    });
+describe('Testing log sign in.', function() {
+  it('It should  return status = 200 ', function(done) {
+    request(app)
+      .post('/sign-in/logSignIn')
+      .send({imageID: 'Image1.jpg'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      done()
   });
+});
+
+
+describe('Testing classification log', function() {
+  it('It should return status = 200', function(done) {
+    request(app)
+      .post('/classification/logClassification')
+      .send({imageID: 'Image1.jpg'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+
+      done()
+  });
+});
+
+describe('Testing classification failure log.', function() {
+  it('It should return with status = 200', function(done) {
+    request(app)
+      .post('/classification-failure/logClassificationFailure')
+      .send({imageID: 'Image1.jpg'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      done()
+  });
+});
