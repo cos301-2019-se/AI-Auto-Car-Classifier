5d9668d52ca3f53df7768ce47ecfa9ad5a1c393a
bestabhinav <37014190+bestabhinav@users.noreply.github.com>
2019-08-19 10:22:02 +0200

    Add files via upload

--- api/logging.js
+++ api/logging.js
@@ -6,7 +6,27 @@ var fs = require('fs');
 router.post('/sign-in', logSignIn);
 router.post('/classification', logClassification);
 router.post('/classification-failure', logClassificationFailure);
+router.post('/logger', logthis);
 
+const db = require('../models/index');
+const Logger = db.sequelize.models.Logger;
+
+
+function logthis(req, res)
+{
+    var user_name=req.body.user;
+    const { user,numberplate,make,model,colour,description,mileage,sold } = req.body;
+      console.log("User name = "+user_name);
+
+     console.log("In function log this");
+     Logger.create({user,numberplate,make,model,colour,description,mileage,sold});
+
+        res.status(200).json({
+
+            message: "Log success"
+
+        });
+}
 
 function logSignIn(req, res)
 {
@@ -46,5 +66,6 @@ function logClassificationFailure(req, res)
 
     });
 }
+
 
 module.exports = router;
