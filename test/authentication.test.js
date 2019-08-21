e19b5301630e5ce8cfdc9e6104d9dc7bf0e3f98b
Keo11 <dkshiko@gmail.com>
2019-08-21 16:41:46 +0200

    auth

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -4,20 +4,25 @@ const app = require("../server.js");
 const auth =  require('../api/authentication');
 
 
-
-let data = {
-  imageID: 'Image1.jpg'
- }
-
-
-describe('Testing log sign in.', function() {
-  it('It should  return status = 200 ', function(done) {
+describe('Testing authentification.', function() {
+  it('Return 401 error code', function(done) {
     request(app)
       .post('/login')
       .send({name: 'Keo', email: 'null@haha.com'})
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(401)
+      done()
+  });
+
+
+  it('Successfully authenticate', function(done) {
+    request(app)
+      .post('/login')
+      .send({name: 'Keo', email: 'null@haha.com'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(success)
       done()
   });
 });
