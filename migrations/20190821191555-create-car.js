f0e58672e2292b29625d39cae6a285ae1c04b4c9
flekhulani <flekhulani@retrorabbit.co.za>
2019-08-21 21:19:35 +0200

    added color to car model

---
+++ migrations/20190821191555-create-car.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      make: {
+        type: Sequelize.STRING
+      },
+      model: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.STRING
+      },
+      color: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      mileage: {
+        type: Sequelize.STRING
+      },
+      vin: {
+        type: Sequelize.STRING
+      },
+      imageURL: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Cars');
+  }
+};
