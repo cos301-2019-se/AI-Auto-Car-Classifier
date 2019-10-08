766e90de4b671ca13398829ee705bcfcbbc0db87
Andrew Jordaan <andyjordaan23@gmail.com>
2019-10-08 17:22:53 +0200

    Changed app configuration

--- app.js
+++ app.js
@@ -4,46 +4,50 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
-
-
 const displayPageRoute = require('./api/displayPage');
 const classifyRoute = require('./api/classification');
 const logRoute = require('./api/logging');
 const authentication = require('./api/authentication');
 let passport = require('./config/passport');
 
-app.use(bodyParser.urlencoded({extended: true, limit: '25mb'}));
-app.use(bodyParser.json({limit: '25mb'}));
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.use(bodyParser.urlencoded({extended: true, limit: '25mb'}));
+    app.use(bodyParser.json({limit: '25mb'}));
 
 //Prevent CORS violation
-app.use(cors());
+    app.use(cors());
 
-app.use(express.static(__dirname + '/frontend')); // so all frontend files can be served
-app.use('/images/', express.static('./images')); // so uploaded images can be served
+    app.use(express.static(__dirname + '/frontend')); // so all frontend files can be served
+    app.use('/images/', express.static('./images')); // so uploaded images can be served
 
-app.use('/classify', classifyRoute);
-app.use('/log', logRoute);
-app.use('/', displayPageRoute);
-app.use('/auth', authentication);
+    app.use('/classify', classifyRoute);
+    app.use('/log', logRoute);
+    app.use('/', displayPageRoute);
+    app.use('/auth', authentication);
 
-app.use(passport.initialize());
+    app.use(passport.initialize());
 //Error handling when url doesn't exist
-app.use((req, res, next) =>
-{
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
+    app.use((req, res, next) =>
+    {
+        const error = new Error('Not found');
+        error.status = 404;
+        next(error);
+    });
 
 //Error handling for thrown error
-app.use((error, req, res, next) =>
-{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
+    app.use((error, req, res, next) =>
+    {
+        res.status(error.status || 500);
+        res.json({
+            error: {
+                message: error.message
+            }
+        });
     });
 });
+
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 //Specify port
 const port = process.env.PORT;
 
-const server = http.createServer(app);
+const server = http.createServer(app.get('port'));
 //Starts Server
 server.listen(port, () => {
     console.log("Listening on port " + port);
