ad47453d1729216f11ab1fbdb6d74f41d079f8a9
Andrew Jordaan <andyjordaan23@gmail.com>
2019-10-08 16:58:11 +0200

    Changed port setting

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
 //Specify port
-const { port }  = require('./config/config');
+const port = process.env.PORT;
 
 const server = http.createServer(app);
 //Starts Server
