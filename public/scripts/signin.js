161e58e2d2c9ecf553c69f9125898e45049336e1
Andrew Jordaan <andyjordaan23@gmail.com>
2019-10-08 20:15:33 +0200

    Changed local url endpoint

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -4,7 +4,7 @@ function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	$.ajax({
         method: "POST",
-        url: "http://localhost:3000/auth/login",
+        url: "/auth/login",
         dataType: "json",
         data: {
 			name: profile.getName(),
